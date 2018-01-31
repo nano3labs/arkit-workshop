95fd4935aea95d4ac14180f97650d7602a2fad74
Michael Yagudaev <michaelyagudaev@gmail.com>
2018-01-30 23:12:01 -0800

    add final lessons

--- LunarLanding/LunarLanding/ViewController.swift
+++ LunarLanding/LunarLanding/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
   @IBOutlet var sceneView: ARSCNView!
   var anchorCount = 0
   var moonNode = SCNNode()
-  var frozen = false
   let button = UIButton(type: .system)
   let trackingStateLabel = UILabel()
   
@@ -45,20 +44,89 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     
     // Set the scene to the view
     sceneView.scene = scene
+    scene.physicsWorld.gravity = SCNVector3(0, -1.622, 0)
     sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
-    
-    scene.rootNode.camera?.name = "Camera"
-    scene.rootNode.light?.name = "Light"
-    
-    button.frame = CGRect(x: 50, y: view.frame.height - 50, width: 100, height: 50)
-    button.setTitle("Freeze", for: .normal)
-    button.addTarget(self, action: #selector(freezeFrame), for: .touchUpInside)
-    sceneView.addSubview(button)
+    sceneView.automaticallyUpdatesLighting = true
     
     trackingStateLabel.frame = CGRect(x: view.frame.width / 2 - (200 / 2), y: 50, width: 200, height: 100)
     trackingStateLabel.numberOfLines = 2
     trackingStateLabel.text = "Learning about surrounding... Try moving camera around"
     sceneView.addSubview(trackingStateLabel)
+    sceneView.isPlaying = true
+    
+    
+    addGestureRecognizers()
+  }
+  
+  func addGestureRecognizers() {
+    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+    view.addGestureRecognizer(tapGestureRecognizer)
+    
+    let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+    view.addGestureRecognizer(panGestureRecognizer)
+  }
+  
+  var selectedNode:SCNNode? = nil
+  @objc func handlePan(gesture: UIPanGestureRecognizer) {
+    let location = gesture.location(in: view)
+    
+    if (gesture.state == .began) {
+      let objectsHit = sceneView.hitTest(location, options: nil)
+      
+      if let objectHit = objectsHit.first {
+        if objectHit.node.name == "apollo-11" {
+          selectedNode = objectHit.node
+        }
+      }
+    } else if (gesture.state == .changed) {
+      guard let m = selectedNode else { return }
+      let hitResult = sceneView.hitTest(location, types: .existingPlaneUsingExtent)
+      if let hitPoint = hitResult.first {
+        let x = hitPoint.worldTransform.columns.3.x
+        let y = hitPoint.worldTransform.columns.3.y + 0.098
+        let z = hitPoint.worldTransform.columns.3.z
+        
+        m.parent!.worldPosition = SCNVector3(x, y, z)
+      } else if (gesture.state == .ended) {
+        selectedNode = nil
+      }
+    }
+  }
+  
+  @objc func handleTap(gesture: UITapGestureRecognizer) {
+    let location = gesture.location(in: view)
+    
+    let hitResult = sceneView.hitTest(location, types: .existingPlaneUsingExtent)
+    
+    if let hitPoint = hitResult.first {
+      let x = hitPoint.worldTransform.columns.3.x
+      let y = hitPoint.worldTransform.columns.3.y
+      let z = hitPoint.worldTransform.columns.3.z
+      
+      let apolloScene = SCNScene(named: "art.scnassets/apollo-11.scn")!
+      let apollo = apolloScene.rootNode.childNode(withName: "apollo-11", recursively: false)!
+      apollo.scale = SCNVector3(0.00034, 0.00034, 0.00034)
+      apollo.rotation = SCNVector4(0, 1, 0, CGFloat.pi / 2)
+      let apolloNode = SCNNode()
+      
+      
+//      apolloNode.position = SCNVector3(x, y + 0.09282, z)
+      apolloNode.position = SCNVector3(x, y + 1.09282, z)
+      
+      let collider = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+      apolloNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(geometry: collider, options: nil))
+      apolloNode.physicsBody?.mass = 1.0
+      apolloNode.physicsBody?.restitution = 0.0
+      apolloNode.physicsBody?.friction = 1.0
+      
+      apolloNode.addChildNode(apollo)
+      
+      let fire = SCNParticleSystem(named: "art.scnassets/fire.scnp", inDirectory: "/")!
+      let emitter = apollo.childNode(withName: "emitter", recursively: true)!
+      emitter.addParticleSystem(fire)
+      
+      sceneView.scene.rootNode.addChildNode(apolloNode)
+    }
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -71,56 +139,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     
     // Run the view's session
     sceneView.session.run(configuration)
-    
-//    let gestureReconginzer = UITapGestureRecognizer(target: self, action: #selector(freezeFrame))
-//    self.view.addGestureRecognizer(gestureReconginzer)
-    
-    let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
-    self.view.addGestureRecognizer(panGestureRecognizer)
-  }
-  
-//  @objc
-//  func moveSphere(gesture: UIPanGestureRecognizer) {
-//    let touchLocation = gesture.location(in: self.view)
-//
-//
-//  }
-  
-  var previousTranslationY:Float = 0.0
-  @objc func handlePan(gesture: UIPanGestureRecognizer) {
-    let translation = Float(gesture.translation(in: view).y)
-    let rotationScale = Float(360.0 / view.frame.height)
-    let yTranslation = Float(translation * rotationScale)
-    let yTranslationDelta = yTranslation - previousTranslationY
-    previousTranslationY = yTranslation
-    
-    if gesture.state == .began || gesture.state == .changed {
-      let rotationAmount = Float(yTranslationDelta)
-      rotateMoon(rotationAmount)
-    } else if gesture.state == .ended {
-      previousTranslationY = 0 // reset translations after pan ends
-    }
-  }
-  
-  func rotateMoon(_ rotationAmount: Float) {
-    let rotateX = SCNMatrix4MakeRotation(GLKMathDegreesToRadians(rotationAmount), 1, 0, 0)
-    moonNode.transform = SCNMatrix4Mult(rotateX, moonNode.transform)
-  }
-  
-  @objc
-  func freezeFrame(gesture: UITapGestureRecognizer) {
-    if frozen {
-      let configuration = ARWorldTrackingConfiguration()
-      configuration.isLightEstimationEnabled = true
-      configuration.planeDetection = .horizontal
-      sceneView.session.run(configuration)
-      button.setTitle("Freeze", for: .normal)
-    } else {
-      sceneView.session.pause()
-      button.setTitle("Unfreeze", for: .normal)
-    }
-    
-    frozen = !frozen
   }
   
   override func viewWillDisappear(_ animated: Bool) {
@@ -187,11 +205,30 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
     
     let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+    let planeMaterial = SCNMaterial()
+    planeMaterial.diffuse.contents = UIImage(named: "art.scnassets/moon.png")!
+    planeMaterial.normal.contents = UIImage(named: "art.scnassets/moon-normal.png")!
+    planeMaterial.lightingModel = .physicallyBased
+    plane.firstMaterial = planeMaterial
+    
     let planeNode = SCNNode(geometry: plane)
     planeNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.y)
     planeNode.eulerAngles.x = -.pi / 2
-    planeNode.opacity = 0.25
+    planeNode.physicsBody = SCNPhysicsBody.static()
     node.addChildNode(planeNode)
+  }
+  
+  func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+    guard let planeAnchor = anchor as? ARPlaneAnchor,
+      let planeNode = node.childNodes.first,
+      let plane = planeNode.geometry as? SCNPlane
+      else { return }
+    
+    planeNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+    plane.width = CGFloat(planeAnchor.extent.x)
+    plane.height = CGFloat(planeAnchor.extent.z)
+    
+    planeNode.physicsBody?.physicsShape = SCNPhysicsShape()
   }
 }
 
